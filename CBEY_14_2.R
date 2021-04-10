186da3f6c4de500de6a9fd72be225b7f2cd3fa26
Aavni Piparsania <Aavni@Aavni-Air.local>
2021-04-10 11:58:23 -0400

    Changes title to include % reponses for both Extremely and Very and edited the axis titles

--- CBEY_14_2.R
+++ CBEY_14_2.R
@@ -1,11 +1,11 @@
 library(readxl)
-data <- read_xlsx('/Users/taliarubnitz/Desktop/WSUMTRUE14_2.xlsx')
+data <- read_xlsx('/Users/Aavni/Dropbox (Yale_FES)/Siya Test/Data Viz Github/CBEY_Data_Project/WSUMTRUE14_2.xlsx')
 View(data)
 plot(data)
 library(ggplot2)
 library(tidyverse)
 
-p <- ggplot(data = data, aes(x=reorder(Rank),y =Students)) +
+p <- ggplot(data = data, aes(x=Rank,y =Students)) +
   geom_bar(stat="identity",fill = "slategray3") +
   theme_bw() +
   labs(title = "28% of MBA Students Think Environmental Sustainabilty is Extremely Important",
@@ -20,11 +20,13 @@ datal$Rank <- factor(datal$Rank,
 q <- ggplot(data = datal, aes(x=Rank,y =Students)) +
   geom_bar(stat="identity",fill = "slategray3") +
   theme_bw() +
-  labs(title = "28% of MBA Students Think it is Extremely Important for Business Leaders to be Knowledgeable about Environmental Sustainability",
+  labs(title = "70% of MBA Students Think it is Extremely or Very Important for Business Leaders to be Knowledgeable about Environmental Sustainability",
        subtitle = "How important is for business leaders to be knowledgeable about environmental sustainability?",
-       caption = "Souce: CBEY")
+       caption = "Source: CBEY",
+       y = "Number of Students", x= "Response")
 q
 
-ggsave("CBEY_14_2_plot.png",
+
+ggsave("CBEY_14_2_plot(edit).png",
        plot = last_plot(),
        dpi = 300)
